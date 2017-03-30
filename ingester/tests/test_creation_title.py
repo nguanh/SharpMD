12af389fa1fbd5b0a89d28fceef08b693d34f12d
Anh Tuan <Nguyen@carekura.de>
2017-03-30 14:30:26 +0000

    added missing test file from previous commit

---
+++ ingester/tests/test_creation_title.py
@@ -0,0 +1,41 @@
+
+from django.test import TestCase
+
+from ingester.helper import *
+from ingester.creation_functions import create_title
+from ingester.models import *
+
+
+class TestCreateTitle(TestCase):
+
+    def test_single_match(self):
+        cluster.objects.create(id=1, name= "matching title")
+        title = "matching title"
+        matching = {
+            "status": Status.SAFE,
+            "match": Match.SINGLE_MATCH,
+            "id": 1,
+            "reason": None,
+        }
+
+        result = create_title(matching,title,)
+        self.assertEqual(result,1)
+
+    def test_no_match(self):
+        title = "matching title"
+        matching = {
+            "status": Status.SAFE,
+            "match": Match.NO_MATCH,
+            "id": None,
+            "reason": None,
+        }
+
+        test_success = {
+            "cluster": {
+                (1, "matching title"),
+            },
+        }
+        result = create_title(matching,title)
+        self.assertEqual(result, 1)
+        self.assertEqual(cluster.objects.first().name, "matching title")
+
