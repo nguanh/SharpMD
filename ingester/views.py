23d1c39493ee41f5ed0764ef07c6d5e71c81f1ff
Anh Tuan <Nguyen@carekura.de>
2017-04-05 12:27:19 +0000

    log textare is now bigger

--- ingester/views.py
+++ ingester/views.py
@@ -2,10 +2,9 @@ from __future__ import absolute_import, unicode_literals
 from django.shortcuts import get_object_or_404,render
 from .models import Config
 import os
+import tailer
 # Create your views here.
 PROJECT_DIR = os.path.dirname(__file__)
-
-
 
 
 def log(request, config_id):
@@ -14,14 +13,11 @@ def log(request, config_id):
     log_name = config.name.strip().replace(" ", "_")
     log_file = os.path.join(log_dir, "{}.log").format(log_name)
     log_exists = os.path.isfile(os.path.join(log_file))
-    log_text = "No log found!"
-    """
     if log_exists:
         with open(log_file, 'r') as f:
-            log_text = tail(f)
+            log_text = tailer.tail(f, 40)
     else:
         log_text = "No log found!"
-    """
 
     return render(request, 'harvester/admin_log.html',{
             'harvester_name': config.name,
