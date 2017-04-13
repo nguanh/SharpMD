711e904a008eebcd25c09c7e6567add943962688
Anh Tuan <Nguyen@carekura.de>
2017-04-13 23:37:10 +0000

    feat: added keyword matching test : added

--- ingester/matching_functions.py
+++ ingester/matching_functions.py
@@ -2,6 +2,7 @@ from .helper import *
 from .models import *
 from django.db.models import ObjectDoesNotExist
 
+
 def match_author(authors):
     results = []
     # iterate through all authors
@@ -127,5 +128,38 @@ def match_type(pub_type):
     except ObjectDoesNotExist:
         type_obj = publication_type.objects.get(name='misc')
     return type_obj
+
+
+def match_keywords(keywords, url_obj):
+    key_id_list = []
+    if keywords is None:
+        return key_id_list
+    for key in keywords:
+        normalized_keyword = normalize_title(key)
+        key_match = keywordsModel.objects.filter(block_name=normalized_keyword)
+        key_match_count = key_match.count()
+
+        if key_match_count == 0:
+            key_record = keywordsModel.objects.create(main_name=key)
+        elif key_match_count == 1:
+            key_record = keywordsModel.objects.get(block_name=normalized_keyword)
+        else:
+            # find matching alias
+            alias_match = keyword_aliases.objects.filter(alias=key, keyword__block_name=normalized_keyword)
+            alias_match_count = alias_match.count()
+            if alias_match_count == 1:
+                key_record = alias_match.first().keyword
+            else:
+                key_record = keywordsModel.objects.create(main_name=key)
+        #create alias and source
+        alias,create = keyword_aliases.objects.get_or_create(alias=key,keyword=key_record)
+        keyword_alias_source.objects.create(alias=alias, url=url_obj)
+        key_id_list.append(key_record.id)
+
+    return key_id_list
+
+
+
+
 
 
---
+++ ingester/tests/test_matching_keywords.py
@@ -0,0 +1,68 @@
+from django.test import  TransactionTestCase
+
+from ingester.matching_functions import match_keywords
+from ingester.models import local_url, global_url,keyword_aliases,keyword_alias_source,keywordsModel
+
+
+class TestMatchKeyword(TransactionTestCase):
+    def setUp(self):
+        self.gurl=global_url.objects.create(id= 5, domain="http://dummy.de", url="http://dummy.de")
+        self.lurl=local_url.objects.create(id=1, url="blabla", global_url=self.gurl)
+
+    def test_none(self):
+        data = None
+        identifier = match_keywords(data, self.lurl)
+        self.assertEqual(identifier, [])
+
+    def test_empty(self):
+        data=[]
+        identifier = match_keywords(data, self.lurl)
+        self.assertEqual(identifier, [])
+
+    def test_no_match(self):
+        identifier = match_keywords(["He?llo", "World"], self.lurl)
+        self.assertEqual(identifier,[1,2])
+        self.assertEqual(keyword_aliases.objects.get(id=1).alias,"He?llo")
+        self.assertEqual(keyword_aliases.objects.get(id=2).alias, "World")
+        self.assertEqual(keyword_alias_source.objects.get(id=1).test(), [1, 1])
+        self.assertEqual(keyword_alias_source.objects.get(id=2).test(), [2, 1])
+
+    def test_single_match(self):
+        keywordsModel.objects.create(id=3,main_name="Key?Wor!d..", block_name="keyword")
+        data = ["Keyword", "Nonsense"]
+        identifier = match_keywords(data, self.lurl)
+
+        self.assertEqual(identifier, [3,4])
+        self.assertEqual(keyword_aliases.objects.get(id=1).alias, "Keyword")
+        self.assertEqual(keyword_aliases.objects.get(id=2).alias, "Nonsense")
+        self.assertEqual(keyword_alias_source.objects.get(id=1).test(), [1, 1])
+        self.assertEqual(keyword_alias_source.objects.get(id=2).test(), [2, 1])
+
+    def test_multi_match_single_alias(self):
+        med1 = keywordsModel.objects.create(main_name="myJournal", block_name="myjournal")
+        med2 = keywordsModel.objects.create(main_name="myJournal", block_name="myjournal")
+        keyword_aliases.objects.create(keyword=med1, alias="myJournal?")
+        keyword_aliases.objects.create(keyword=med2, alias="myJournal")
+
+        identifier = match_keywords(["myJournal"], self.lurl)
+
+        self.assertEqual(identifier, [2])
+        self.assertEqual(keyword_alias_source.objects.get(id=1).test(),[2,1])
+
+    def test_multi_match_multi_alias(self):
+        med1 = keywordsModel.objects.create(main_name="myJournal", block_name="myjournal")
+        med2 = keywordsModel.objects.create(main_name="myJournal", block_name="myjournal")
+        keyword_aliases.objects.create(keyword=med1, alias="myJournal")
+        keyword_aliases.objects.create(keyword=med2, alias="myJournal")
+        # sanity check
+        self.assertEqual(keywordsModel.objects.count(),2)
+        identifier = match_keywords(["myJOurnal"], self.lurl)
+
+        self.assertEqual(identifier, [3])
+        self.assertEqual(keyword_alias_source.objects.get(id=1).test(), [3,1])
+
+
+
+
+
+
