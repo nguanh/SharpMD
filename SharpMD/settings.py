9624af8534debfc8e5eb6384ba7609027e90ce87
Anh Tuan <Nguyen@carekura.de>
2017-04-21 10:50:38 +0000

    feat: added mysql config file

--- SharpMD/settings.py
+++ SharpMD/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -99,6 +98,9 @@ DATABASES = {
         'NAME': "storage",
         'ENGINE': 'django.db.backends.mysql',
         'PORT': '',
+        'OPTIONS':{
+            'read_default_file': os.path.join(BASE_DIR,"my.cnf"),
+        },
 
     }
 }
