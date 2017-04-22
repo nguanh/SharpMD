f2bcd6b6fc47f584285a60222ae7515f91465782
nguanh <phu.nguyen@student.uni-luebeck.de>
2017-04-22 17:54:01 +0200

    feat: ingester task checks if same task is already running

--- ingester/tasks.py
+++ ingester/tasks.py
@@ -5,10 +5,24 @@ from celery.exceptions import Ignore
 from celery import states
 from .exception import IIngester_Exception,IIngester_Disabled
 from .ingest_task import ingest_task
+from SharpMD.celery import app
 
 
 @shared_task
 def ingestsource(package, class_name,config_id):
+    # Check if task is already running by checking queue for task with same parameters
+    active_queue = app.control.inspect().active()["celery@bremen"]
+    # transform parameter to string list for comparision
+    parameter_list = "['{}', '{}', {}]".format(package, class_name, config_id)
+    matches = 0
+    for active_task in active_queue:
+        if active_task["args"] == parameter_list:
+            matches += 1
+
+    if matches > 1:
+        print("Task {} is already running, skipping execution".format(parameter_list))
+        return None
+
     try:
         ingest_task(package, class_name, config_id)
 
