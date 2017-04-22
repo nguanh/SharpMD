2a3bb1608fc2bc43035ce7f00c468c8d57bcb81c
Anh Tuan <Nguyen@carekura.de>
2017-04-22 20:45:26 +0000

    feat: added script file for full text indexes

--- scripts/reset_django.sh
+++ scripts/reset_django.sh
@@ -1,9 +1,11 @@
 cd /home/nguyen/SharpMD
-echo 'Reset Django Database' 
+echo 'Reset Django Database....'
 python manage.py reset_db --router=default
-echo 'Creating Django Tables'
+echo 'Creating Django Tables...'
 python manage.py migrate
-echo 'Importing initial database records'
+echo 'Adding full text _index...'
+mysql storage < create_fulltext_index.sql
+echo 'Importing initial database records...'
 python manage.py loaddata initial_data.json
-echo 'Creating Admin accounts'
+echo 'Creating Admin accounts...'
 python manage.py createsuperuser
