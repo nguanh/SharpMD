fdebee0aba918a4dfc9702b69e9a77bb6a4104a0
Anh Tuan <Nguyen@carekura.de>
2017-04-23 12:18:29 +0000

    feat: updated django reset script to include full text indexes

--- scripts/reset_django.sh
+++ scripts/reset_django.sh
@@ -4,7 +4,7 @@ python manage.py reset_db --router=default
 echo 'Creating Django Tables...'
 python manage.py migrate
 echo 'Adding full text _index...'
-mysql storage < create_fulltext_index.sql
+mysql -u root -p storage< create_fulltext_index.sql
 echo 'Importing initial database records...'
 python manage.py loaddata initial_data.json
 echo 'Creating Admin accounts...'
