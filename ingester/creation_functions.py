22a798a0df9dc3761cf52e1c225342df5191ec2b
Anh Tuan <Nguyen@carekura.de>
2017-04-30 22:22:36 +0000

    duplication error test

--- ingester/creation_functions.py
+++ ingester/creation_functions.py
@@ -111,7 +111,7 @@ def create_publication(g_url, cluster_obj, author_objs, type_obj=None, pub_mediu
                                           medium=pub_medium_obj, type=type_obj)
         publication_data = publication.objects.create(url=url_id, cluster=cluster_obj)
         for priority, author_obj in enumerate(author_objs):
-            pub_aut_list.append(publication_author(url=url_id, author=author_obj, defaults={'priority': priority}))
+            pub_aut_list.append(publication_author(url=url_id, author=author_obj, priority= priority))
         for keyword in keyword_objs:
             keyword_list.append(publication_keyword(url=url_id, keyword=keyword))
         publication_author.objects.bulk_create(pub_aut_list)
