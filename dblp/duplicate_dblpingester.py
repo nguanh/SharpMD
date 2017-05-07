7ed122b0772022fe272b0dc4c611b69acefe881e
Anh Tuan <Nguyen@carekura.de>
2017-05-07 18:30:37 +0000

    feat: updated duplicate ingester

--- dblp/duplicate_dblpingester.py
+++ dblp/duplicate_dblpingester.py
@@ -7,6 +7,9 @@ class DuplicateDblpIngester(DblpIngester):
     This ingester allows ingesting records that are already included by adding the 'XX' prefix to every local_url
     this allows simulating test cases, where publications from different sources refer to the same publication
     """
+    def __init__(self, name, harvesterdb=None):
+        DblpIngester.__init__(self,name,harvesterdb)
+        self.query = ("SELECT * FROM {}.dblp_article WHERE last_harvested != 0").format(self.harvester_db)
 
     def mapping_function(self, query_tuple):
         mapping = super(DblpIngester, self).mapping_function(query_tuple)
