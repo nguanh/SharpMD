b913ede1ddcf7de843783ac7dc9890e5f6205a4d
Anh Tuan <Nguyen@carekura.de>
2017-05-19 11:12:18 +0000

    feat: improvied search functionality

--- ingester/filters.py
+++ ingester/filters.py
@@ -2,7 +2,10 @@ from .models import *
 import django_filters
 
 class PublicationFilter(django_filters.FilterSet):
+    authors__block_name = django_filters.CharFilter(lookup_expr='icontains', label="Author")
+    publication__title  = django_filters.CharFilter(lookup_expr='icontains', label="Title")
     class Meta:
         model = local_url
         fields = ['publication__title','authors__block_name']
+
 
